1f39d47d413453a7cdee6871dea441b887a328b8
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2024-02-06 18:48:15 +0300

    Initial Commit

---
+++ iCalculate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCalculate
+//
+//  Created by SHUAA on 6.2.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCalculate/iCalculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculateApp.swift
+//  iCalculate
+//
+//  Created by SHUAA on 6.2.2024.
+//
+
+import SwiftUI
+
+@main
+struct iCalculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
